45e58a763105ae0345bfbf720ec1093e816e7574
Ayikanying <Ayikanying@users.noreply.github.com>
2021-11-18 00:07:53 +0300

    adding database

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -79,11 +79,25 @@ WSGI_APPLICATION = 'content_aggregator.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'd8r4mm3v4oq0fv',
+
+        'USER': 'muotxrhuylcnop',
+
+        'PASSWORD': 'ebaa0af2df362f5d3f9ec0baeec069e6ca97a5e9f43888f1647158f3e4bfa822',
+
+        'HOST': 'ec2-54-146-82-179.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+
     }
+
 }
+
 
 
 # Password validation
